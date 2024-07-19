90a1f67d5d6e2e5c0046e76422a8048143dc8fad
Z0maxx <nyari.zoltan500@gmail.com>
2024-07-19 08:25:29 +0200

    added types

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-import { Side, Orientation, CubeLayer, Direction, Cube, Layer } from "./cube-constants"
+import { Side, Orientation, CubeLayer, Direction, Cube, Layer, ColorNumber, Notation } from "./cube-constants"
 
 export type TSide = typeof Side[keyof typeof Side]
 
@@ -12,6 +12,12 @@ export type TCube = typeof Cube[keyof typeof Cube]
 
 export type TLayer = typeof Layer[keyof typeof Layer]
 
+export type TColorNumber = typeof ColorNumber[keyof typeof ColorNumber]
+
+export type TNotation = typeof Notation[keyof typeof Notation]
+
+export type ColorString = 'red' | 'orange' | 'blue' | 'white' | 'green' | 'yellow' | 'black'
+
 export type Move = {
     targetCube: TCube
     targetSides: Array<TSide>
@@ -19,9 +25,72 @@ export type Move = {
     originSides: Array<TSide>
 }
 
-export type LayerMove = {
+export type MoveWithLayer = Move & {
     targetLayer: TLayer
     originLayer: TLayer
 }
 
-export type MoveWithLayer = Move & LayerMove
+export type TranslatedNotation = {
+    layer: boolean
+    twice: boolean
+    direction: TDirection
+    cubeLayer?: TCubeLayer
+}
+
+export type SelectedVideo = 'normal' | 'avg-corrected' | 'max-corrected' | 'p-norm-corrected' | 'auto-detect' | 'auto-detect-start'
+
+export type AvgColorWorkerMessage = {
+    array: Uint8ClampedArray
+    size: number
+    color: number
+}
+
+export type CorrectionWorkerCanvases = {
+    normalCanvas: OffscreenCanvas
+    correctedCanvas: OffscreenCanvas
+    width: number
+    height: number
+}
+
+export type IdentifierWorkerCanvases = {
+    normalCanvas: OffscreenCanvas
+    correctedCanvas: OffscreenCanvas
+    resultCanvas: OffscreenCanvas
+    width: number
+    height: number
+}
+
+export type WorkerCorrection = {
+    bitmap: ImageBitmap
+    selectedVideo: SelectedVideo
+}
+
+export type CorrectionWorkerMessage = CorrectionWorkerCanvases | WorkerCorrection | SelectedVideo | number
+
+export type CorrectionWorkerResponse = Uint8ClampedArray | SelectedVideo | number
+
+export type IdentifierWorkerMessage = IdentifierWorkerCanvases | Uint8ClampedArray
+
+export type CubeResultImage = {
+    pixels: Array<number>
+    colors: Array<string>
+    resultWidth: number
+    resultHeight: number
+}
+
+export type CornerColoredSides = {
+    layer: TLayer
+    cube: TCube
+    sides: [TSide, TSide, TSide]
+}
+
+export type EdgeColoredSides = {
+    layer: TLayer
+    cube: TCube
+    sides: [TSide, TSide]
+}
+
+export type CubeSolve = {
+    moves: Array<TNotation>
+    error: string
+}
